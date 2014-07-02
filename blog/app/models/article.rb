6cecc09029b091d2a6c39f0f6c579e2cad8bfc7c
steve weber <steve@localhost.localdomain>
2014-07-02 23:44:46 +0200

    got the papaerclip image uploads working

--- blog/app/models/article.rb
+++ blog/app/models/article.rb
@@ -2,8 +2,11 @@ class Article < ActiveRecord::Base
 
 	has_many :comments, dependent: :destroy
 
-	has_attached_file :image
-
+	
 	validates :title, presence: true,
                     length: { minimum: 5 }
+
+	has_attached_file :image
+  	
+	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
